0478c5414b77d3943918eaee9b6c66465938c469
Roger Fan <rogerjoeyfan@gmail.com>
2023-12-17 16:35:52 -0800

    set up redirect on register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from 'next';
-
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
     title: 'Register | GunnHacks 9.0'
 }
 
 export default function Register() {
-    return (
-        <div className="flex justify-center">
-            <iframe
-                src="https://docs.google.com/forms/d/e/1FAIpQLSdF_KFH0pH_ryAx3uYglID2ITeXvoTaHfTh2i9YMgnlwLC91A/viewform?usp=sf_link&embedded=true"
-                className="absolute border-none w-full h-full inset-0 mx-auto"
-            >
-                Loading…
-            </iframe>
-        </div>
-    )
+    redirect("https://docs.google.com/forms/d/e/1FAIpQLSdF_KFH0pH_ryAx3uYglID2ITeXvoTaHfTh2i9YMgnlwLC91A/viewform");
+    // return (
+    //     <div className="flex justify-center">
+    //         <iframe
+    //             src="https://docs.google.com/forms/d/e/1FAIpQLSdF_KFH0pH_ryAx3uYglID2ITeXvoTaHfTh2i9YMgnlwLC91A/viewform?usp=sf_link&embedded=true"
+    //             className="absolute border-none w-full h-full inset-0 mx-auto"
+    //         >
+    //             Loading…
+    //         </iframe>
+    //     </div>
+    // )
 }
