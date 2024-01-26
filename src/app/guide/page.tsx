d70d7488f1ffcbe4eddef35a09628ca84ab25c39
Roger Fan <rogerjoeyfan@gmail.com>
2024-01-25 17:19:55 -0800

    team form

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
-    title: 'Hacker Guide | GunnHacks 9.0'
+    title: 'Hacker Guide | GunnHaX'
 }
 
 export default function Guide() {
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
-    title: 'Register | GunnHacks 9.0'
+    title: 'Register | GunnHaX'
 }
 
 export default function Register() {
