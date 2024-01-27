9ca61f6355b2f6d4f05caa93f862d1669dfac430
Roger Fan <rogerjoeyfan@gmail.com>
2024-01-27 14:15:49 -0800

    update schedule

--- src/app/(landing)/Schedule.tsx
+++ src/app/(landing)/Schedule.tsx
@@ -11,12 +11,14 @@ export default function Schedule() {
                     <ScheduleItem start="2:00 PM" end="2:30 PM">Opening ceremony</ScheduleItem>
                     <ScheduleItem start="2:30 PM" end="3:00 PM">Team formation</ScheduleItem>
                     <ScheduleItem start="3:00 PM" bold>Commence Hacking!</ScheduleItem>
-                    <ScheduleItem start="3:15 PM" end="4:45 PM">Intro to Code</ScheduleItem>
-                    <ScheduleItem start="5:00 PM" end="6:30 PM">Basic AI/ML</ScheduleItem>
-                    <ScheduleItem start="5:00 PM" end="6:30 PM">Github Copilot Workshop by MLH</ScheduleItem>
+                    <ScheduleItem start="3:15 PM" end="4:45 PM">Intro to Code WS</ScheduleItem>
+                    <ScheduleItem start="3:15 PM" end="4:45 PM">Intro to Unity WS</ScheduleItem>
+                    <ScheduleItem start="5:00 PM" end="6:30 PM">Basic AI/ML WS</ScheduleItem>
+                    <ScheduleItem start="5:00 PM" end="6:30 PM">Github Copilot WS by MLH</ScheduleItem>
                     <ScheduleItem start="6:30 PM">Dinner</ScheduleItem>
                     <ScheduleItem start="7:00 PM" end="7:30 PM">Coding Trivia Kahoot</ScheduleItem>
-                    <ScheduleItem start="7:30 PM" end="8:45 PM">Web Dev/Discord Bots Workshop</ScheduleItem>
+                    <ScheduleItem start="7:30 PM" end="8:45 PM">Web Development WS</ScheduleItem>
+                    <ScheduleItem start="7:30 PM" end="8:45 PM">Discord Bots WS</ScheduleItem>
                     <ScheduleItem start="10:00 PM" end="10:30 PM">Cow Drawing Competition</ScheduleItem>
                     <ScheduleItem start="10:00 PM" end="12:00 AM">Movie Night</ScheduleItem>
                 </ScheduleDay>
